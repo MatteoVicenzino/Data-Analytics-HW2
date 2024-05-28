c29a348840fc4dbc962d3df47faa3580f481adba
samueleserri <115220983+samueleserri@users.noreply.github.com>
2024-05-28 14:40:02 +0200

    Update funzione_variabili_quantitative.r
    funzione che mostra summary, varianza e st dev

--- ScriptR/funzione_variabili_quantitative.r
+++ ScriptR/funzione_variabili_quantitative.r
@@ -1,7 +1,7 @@
 #questa funzione prende in input una variabile quantitativa e stampa summary + varianza e dev. standared in una riga
 #!!omette gli NA ovunque
 display_summary_and_var <- function(variabile){
-  c(na.omit(summary(variabile)), 
+  c(summary(variabile), 
         var = var(variabile, na.rm = T), 
         sd = sd(variabile, na.rm = T))
 }
