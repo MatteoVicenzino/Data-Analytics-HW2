0cb2f9ea03fb9a0f0443f8cb4cfc772ea553a9af
MatteoVicenzino <115220969+MatteoVicenzino@users.noreply.github.com>
2024-05-31 18:21:53 +0200

    Update Grafico_colorato.R

--- ScriptR/Grafico_colorato.R
+++ ScriptR/Grafico_colorato.R
@@ -1,2 +1,2 @@
 # grafico colorato
-ggplot(case, aes(x = as.factor(MasVnrType), y = SalePrice, fill = as.factor(MasVnrType))) + geom_violin() + geom_boxplot(alpha=1/5) + guides(fill = FALSE)
+ggplot(case, aes(x = as.factor(MasVnrType), y = SalePrice, fill = as.factor(MasVnrType))) + geom_violin() + geom_boxplot(width=0.2, alpha=1/5) + guides(fill = FALSE)
