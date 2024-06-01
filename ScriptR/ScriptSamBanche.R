da27498a6e7aaeedeffb10a0d1d366d43832c976
samueleserri <115220983+samueleserri@users.noreply.github.com>
2024-06-01 17:22:44 +0200

    Update ScriptSamBanche.R

--- ScriptR/ScriptSamBanche.R
+++ ScriptR/ScriptSamBanche.R
@@ -1,20 +1,21 @@
 #month_inactive
-display_summary_and_var_qualitative(banche$Months_Inactive_12_mon)
-
+banche$Months_Inactive_12_mon <-factor(banche$Months_Inactive_12_mon)
+display_table(banche$Months_Inactive_12_mon, titolo = 'Month inactive 12 month')
+#si osserva una maggiore concentrazione di valori in 2 e 3
 #contacts count 12 month
-display_summary_and_var_qualitative(banche$Contacts_Count_12_mon)
-
+banche$Contacts_Count_12_mon <- factor(banche$Contacts_Count_12_mon)
+display_table(banche$Contacts_Count_12_mon, titolo = 'contacts 12 month')
+#anche ui si osserva una maggiore concentrazione di valori maggiore in 2 e 3, questa variabile è meno assimetrica della precedente
 #le prime 2 presentano solo i valori da 0 a 6 e faccio il barplot 
 
 #credit limit
 display_summary_and_var(banche$Credit_Limit)
-skewness(banche$Credit_Limit)
 hist(banche$Credit_Limit, freq = T, main = "distribuzione credito limite")
-#fortemente assimetrica
+boxplot(banche$Credit_Limit)
+#la distribuzione è fortemente assimetrica 
 
 #total revolving bal
 display_summary_and_var(banche$Total_Revolving_Bal)
-skewness(banche$Total_Revolving_Bal)
 hist(banche$Total_Revolving_Bal, freq = T, main = "Total Revolving Bal")
 boxplot(banche$Total_Revolving_Bal)
 #la moda è 0
@@ -22,5 +23,4 @@ boxplot(banche$Total_Revolving_Bal)
 
 #avg open to buy
 display_summary_and_var(banche$Total_Revolving_Bal)
-skewness(banche$Avg_Open_To_Buy)
 hist(banche$Avg_Open_To_Buy ,freq = T, main = "distribuzione avg_open_to_buy")
