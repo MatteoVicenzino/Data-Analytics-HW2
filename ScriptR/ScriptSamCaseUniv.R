e9ad7060524e2028a27cd6e98a5b4aab8d9e56b7
samueleserri <115220983+samueleserri@users.noreply.github.com>
2024-06-01 17:37:38 +0200

    final

--- ScriptR/ScriptSamCaseUniv.R
+++ ScriptR/ScriptSamCaseUniv.R
@@ -5,7 +5,7 @@ case <- train[, 42:61]
 #variabile che rappresenta l'assenza o la presenza del central air conditioning
 
 case$CentralAir <- factor(case$CentralAir)
-display_table(case$CentralAir)
+display_table(case$CentralAir, titolo = 'central air conditioning')
 
 #si osserva che il 93% circa delle case ha il central air conditioning
 
@@ -13,7 +13,7 @@ display_table(case$CentralAir)
 #variabile che rappresenta il sistema elettrico
 
 case$Electrical <- factor(case$Electrical)
-display_table(case$Electrical)
+display_table(case$Electrical, titolo = 'sistema elettrico')
 
 #vi sono 4 tipi diversi, il 91% delle case ha il circuito standard, il restante 9% si dovide negli altri tre tipi
 #il 6% è rappresentato dal tipo FuseA
@@ -21,7 +21,6 @@ display_table(case$Electrical)
 #superficie del primo piano
 
 display_summary_and_var(case$X1stFlrSF)
-skewness(case$X1stFlrSF)
 hist(case$X1stFlrSF, probability = T, main = 'superficie primo piano')
 
 #la maggior parte delle case si trova nel range 800-1200 piedi quadrati
@@ -29,7 +28,6 @@ hist(case$X1stFlrSF, probability = T, main = 'superficie primo piano')
 #superficie secondo piano
 
 display_summary_and_var(case$X2ndFlrSF)
-skewness(case$X2ndFlrSF)
 hist(case$X2ndFlrSF, probability = T, main = 'superficie secondo piano')
 
 #la maggior parte delle case (il 56 %) non possiede un secondo piano, prendendo in considerazione solo quelle che lo hanno
@@ -49,7 +47,6 @@ length(case$LowQualFinSF[case$LowQualFinSF == 0])
 #metri quadri sopra il terreno
 
 display_summary_and_var(case$GrLivArea)
-skewness(case$GrLivArea)
 hist(case$GrLivArea, probability = T)
 boxplot(case$GrLivArea)
 points(mean(case$GrLivArea), col = 'red')
@@ -60,35 +57,35 @@ points(mean(case$GrLivArea), col = 'red')
 #numero basement full bathroom
 
 case$BsmtFullBath <- factor(case$BsmtFullBath)
-display_table(case$BsmtFullBath)
+display_table(case$BsmtFullBath, titolo = 'Basement full bath')
 
 #presenta solo i caratteri 0, 1 , 2 e 3. il 59% delle case ha 0, il 40% 1
 
 #numero basement half bathroom
 
 case$BsmtHalfBath <- factor(case$BsmtHalfBath)
-display_table(case$BsmtHalfBath)
+display_table(case$BsmtHalfBath, titolo = 'Basement half bath')
 
 #presenta i caratteri 0, 1 e 2, il 94% delle case ha 0, il 5% 1
 
 #numero camere sopra il livello del suolo, non include le camerdel basement.
 
 case$BedroomAbvGr <- factor(case$BedroomAbvGr)
-display_table(case$BedroomAbvGr)
+display_table(case$BedroomAbvGr, titolo = 'Bedroom above grade')
 
 #il 55% delle case ne ha 3. 
 
 #numero di cucine 
 
 case$KitchenAbvGr <- factor(case$KitchenAbvGr)
-display_table(case$KitchenAbvGr)
+display_table(case$KitchenAbvGr, 'Kitchen above grade')
 
 #il 95% delle case ne ha 1
 
 #qualità delle cucine
 
 case$KitchenQual <- factor(case$KitchenQual, levels = c('Ex', 'Fa', 'Gd', 'TA', 'Po'))
-display_table(case$KitchenQual)
+display_table(case$KitchenQual, titolo = 'Kitchen quality')
 
 #il 50% delle cucine è a un livello Average e il 40% a un livelo good
 
@@ -96,7 +93,7 @@ display_table(case$KitchenQual)
 
 display_summary_and_var(case$TotRmsAbvGrd)
 rbind(tot = table(case$TotRmsAbvGrd),
-prop = prop.table(table(case$TotRmsAbvGrd)))
+      prop = prop.table(table(case$TotRmsAbvGrd)))
 boxplot(case$TotRmsAbvGrd)
 
 #il 28% ha 6 stanze, #il 22% 7
@@ -104,28 +101,28 @@ boxplot(case$TotRmsAbvGrd)
 #home functionality
 
 case$Functional <- factor(case$Functional)
-display_table(case$Functional)
+display_table(case$Functional, titolo = 'home functionality')
 
 #il 93% delle case presenta typical functionality
 
 #fire places
 
 case$Fireplaces <- factor(case$Fireplaces)
-display_table(case$Fireplaces)
+display_table(case$Fireplaces, titolo = 'Fireplaces')
 
 #il 47% delle case ha 0, il 44% ha 1
 
 #qualità fire place
 
 case$FireplaceQu <- factor(case$FireplaceQu)
-display_table(case$FireplaceQu)
+display_table(case$FireplaceQu, titolo = 'fireplace quality')
 
 #il 41% ha una qualità average, il 49% ha una qualità good
 
 #posizione garage
 
 case$GarageType <- factor(case$GarageType)
-display_table(case$GarageType)
+display_table(case$GarageType, titolo = 'Garage type')
 
 #il 63% ha il garage affiancato alla casa
 
@@ -133,7 +130,7 @@ display_table(case$GarageType)
 
 case$GarageFinish <- factor(replace(case$GarageFinish, is.na(case$MSSubClass), "Non Presente"), 
                             levels = c('Fin', 'RFn', 'Unf', 'NA'))
-display_table(case$GarageFinish)
+display_table(case$GarageFinish, titolo = 'Garage finish')
 
 #il 43% delle case ha il garage non finito
 
@@ -143,3 +140,5 @@ display_table(case$GarageFinish)
 hist(case$GarageYrBlt, probability = T,breaks= c(5*0:28)+1870,  main = 'anno costruzione garage')
 abline(v = median(case$GarageYrBlt, na.rm = T),lwd = 1, col = "red")
 abline(v = mean(case$GarageYrBlt, na.rm = T),lwd = 1)
+
+# si not aun calo tra il 1980 e il 1990 e un picco nei primi anni del 2000
